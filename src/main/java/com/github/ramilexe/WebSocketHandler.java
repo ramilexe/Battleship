16a9a8791441ccf9dba27759d72b1e9df43b2e49
ramilexe <ramilexe@gmail.com>
2014-11-18 13:50:07 +0300

    websocket handshake

--- src/main/java/com/github/ramilexe/WebSocketHandler.java
+++ src/main/java/com/github/ramilexe/WebSocketHandler.java
@@ -1,13 +1,22 @@
 package com.github.ramilexe;
 
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshaker;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshakerFactory;
+import io.netty.util.CharsetUtil;
 
+import static io.netty.handler.codec.http.HttpHeaders.Names.*;
 import static io.netty.handler.codec.http.HttpResponseStatus.*;
+import static io.netty.handler.codec.http.HttpVersion.*;
+import static io.netty.handler.codec.http.HttpMethod.*;
 
 public class WebSocketHandler extends SimpleChannelInboundHandler<Object> {
     @Override
@@ -21,14 +30,56 @@ public class WebSocketHandler extends SimpleChannelInboundHandler<Object> {
 
     private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest request) {
         if (!request.getDecoderResult().isSuccess()) {
-            //sendHttpResponse(ctx, request, new DefaultFullHttpResponse())
+            sendHttpResponse(ctx, request, new DefaultFullHttpResponse(HTTP_1_1, BAD_REQUEST));
+            return;
         }
 
-        sendHttpResponse(ctx, request, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, PAYMENT_REQUIRED));
+        if (request.getMethod() != GET) {
+            sendHttpResponse(ctx, request, new DefaultFullHttpResponse(HTTP_1_1, FORBIDDEN));
+            return;
+        }
+
+        //allow only / uri
+        if (!request.getUri().equals("/")) {
+            sendHttpResponse(ctx, request, new DefaultFullHttpResponse(HTTP_1_1, NOT_FOUND));
+            return;
+        }
+
+        WebSocketServerHandshakerFactory factory = new WebSocketServerHandshakerFactory(getWebSocketLocation(request), null, false);
+        WebSocketServerHandshaker handshaker = factory.newHandshaker(request);
+        if (handshaker == null) {
+            WebSocketServerHandshakerFactory.sendUnsupportedWebSocketVersionResponse(ctx.channel());
+            return;
+        }
+        handshaker.handshake(ctx.channel(), request);
     }
 
     private void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest request, FullHttpResponse res) {
+        // Generate an error page if response getStatus code is not OK (200).
+        int code = res.getStatus().code();
+        if (code != 200) {
+            String errorMessage = "<h1>" + res.getStatus().reasonPhrase() + " "  + Integer.toString(code) + "</h1>";
+            ByteBuf buf = Unpooled.copiedBuffer(errorMessage, CharsetUtil.UTF_8);
+            res.content().writeBytes(buf);
+            buf.release();
+            HttpHeaders.setContentLength(res, res.content().readableBytes());
+        }
+
         ChannelFuture f = ctx.channel().writeAndFlush(res);
+
+        if (!HttpHeaders.isKeepAlive(request) || res.getStatus().code() != 200) {
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
     }
 
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.err.println("Exception: " + cause.getMessage());
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    private static String getWebSocketLocation(FullHttpRequest req) {
+        return "ws://" + req.headers().get(HOST);
+    }
 }
